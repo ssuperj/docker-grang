c77ad49f610413db381bca9b3440feaaa3c5677d
ssuperj <poqwer95@naver.com>
2023-03-03 07:20:41 +0900

    init grang docker compose

--- mygrang/src/main/resources/static/js/detail.js
+++ mygrang/src/main/resources/static/js/detail.js
@@ -5,7 +5,7 @@ const currentHostname = window.location.hostname;
 async function initPage() {
     let sendUser = (await findUser())[0];
     let recvUser = (await findUser())[1];
-    const eventSource = new EventSource(`http://${currentHostname}:8100/sender/${sendUser.username}/receiver/${recvUser.username}`);
+    const eventSource = new EventSource(`http://localhost:8100/sender/${sendUser.username}/receiver/${recvUser.username}`);
     eventSource.onmessage = (event) => {
         const data = JSON.parse(event.data);
         if(data.sender === sendUser.username) {
@@ -74,7 +74,7 @@ async function addMyMessage() {
         msg: sendBoxInput.value,
     };
 
-    fetch(`http://${currentHostname}:8100/chat`, {
+    fetch(`http://localhost:8100/chat`, {
         method: "post",
         body: JSON.stringify(chat),
         headers: {
